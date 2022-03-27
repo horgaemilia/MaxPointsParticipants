5cc40dc1b408b5ea71c2222d6bbe8d6c7342f176
horgaemilia <emiliahorga15@gmail.com>
2022-03-27 17:18:53 +0300

    added before and after behaviour

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -3,19 +3,31 @@ package ssvv.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import ssvv.example.domain.Nota;
 import ssvv.example.domain.Student;
 import ssvv.example.domain.Tema;
-import ssvv.example.repository.NotaXMLRepository;
-import ssvv.example.repository.StudentXMLRepository;
-import ssvv.example.repository.TemaXMLRepository;
+import ssvv.example.repository.*;
 import ssvv.example.service.Service;
 import ssvv.example.validation.NotaValidator;
 import ssvv.example.validation.StudentValidator;
 import ssvv.example.validation.TemaValidator;
 import ssvv.example.validation.Validator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.PrintWriter;
 
 /**
  * Unit test for simple App.
@@ -25,7 +37,39 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    private Validator<Student> studentValidator = new StudentValidator();
+    private Validator<Tema> temaValidator = new TemaValidator();
+    private Validator<Nota> notaValidator = new NotaValidator();
+    private StudentXMLRepository fileRepository1 ;
+    private TemaXMLRepository fileRepository2 ;
+    private NotaXMLRepository fileRepository3 ;
+    private Service service ;
 
+    @Before
+    public void before()
+    {
+        String studentFileName = "studenti.xml";
+        String notaFileName = "note.xml";
+        String temaFileName = "teme.xml";
+
+        fileRepository1 = new StudentXMLRepository(studentValidator,studentFileName);
+        fileRepository2 = new TemaXMLRepository(temaValidator,temaFileName);
+        fileRepository3 = new NotaXMLRepository(notaValidator,notaFileName);
+        service = new Service(fileRepository1,fileRepository2,fileRepository3);
+    }
+
+    @After
+    public void after()
+    {
+        try {
+            PrintWriter writer = new PrintWriter("studenti.xml");
+            writer.print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                    "<Entitati>");
+            writer.println("</Entitati>");
+            writer.close();
+        }
+        catch (Exception ignored){}
+    }
 
     @Test
     public void shouldAnswerWithTrue()
@@ -37,12 +81,6 @@ public class AppTest
     public void addStudentEmptyIdShouldReturnError()
     {
         Student dummyStudent = new Student(null,"gigel",129);
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
         Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         try {
             service.saveStudent(dummyStudent.getID(),dummyStudent.getNume(),dummyStudent.getGrupa());
@@ -58,13 +96,6 @@ public class AppTest
     public void addStudentValidFieldsShouldAddStudent()
     {
         Student dummyStudent = new Student("100","gigel",129);
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         assertEquals(1,service.saveStudent(dummyStudent.getID(),dummyStudent.getNume(),dummyStudent.getGrupa()));
     }
 }
